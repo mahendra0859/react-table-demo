14e3fffee3bceb350d699e12f7dbc984477c7332
Mahendra AR <mahendra0859@gmail.com>
2020-10-20 09:28:42 +0530

    Updates

---
+++ precache-manifest.ae75c89220b3ae1a4147bca5744ff1e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53aa22fe85e77ec7ad60081988663b94",
+    "url": "/react-table-demo/index.html"
+  },
+  {
+    "revision": "5e02a43e84441a5f355b",
+    "url": "/react-table-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0ddcbe43c48864809bb",
+    "url": "/react-table-demo/static/js/2.51283158.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-table-demo/static/js/2.51283158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e02a43e84441a5f355b",
+    "url": "/react-table-demo/static/js/main.a6b28f7d.chunk.js"
+  },
+  {
+    "revision": "39e21eaac22b77ea4380",
+    "url": "/react-table-demo/static/js/runtime-main.c4e71d75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-table-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
